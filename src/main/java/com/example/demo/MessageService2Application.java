1929c007433632e16be36b29c7ec70e8c24e935f
fatmag8 <fatmagezerr1997@gmail.com>
2020-09-22 10:30:52 +0300

    Initial commit

---
+++ src/main/java/com/example/demo/MessageService2Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageService2Application.class, args);
+	}
+
+}
